6f98d68f38acee89a6cb7bcee29db7989465af40
Ivan <1095036273@qq.com>
2021-04-07 11:36:48 +0800

    修改目录名

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import { createApp } from 'vue'
-import './app.scss'
-import store from "./vuex/index"
-import { systmeInfo } from './utils/envInfo'
-import 'promise-prototype-finally'
+import { createApp } from "vue";
+import "./app.scss";
+import store from "./store/index";
+import { systmeInfo } from "./utils/envInfo";
+import "promise-prototype-finally";
 
 const App = createApp({
-  onShow(options) { },
+  onShow(options) {},
   mounted() {
-    console.log(systmeInfo)
+    console.log(systmeInfo);
   }
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
-})
-App.use(store)
-export default App
+});
+App.use(store);
+export default App;
